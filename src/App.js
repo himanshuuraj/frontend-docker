820d508112ec2b0a6df6120428702ec03cc68adc
himanshurraj <61135023+himanshurraj@users.noreply.github.com>
2020-07-04 17:49:31 +0530

    changed travis file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("HELLO");
+  console.log("HELLO0");
   return (
     <div className="App">
       <header className="App-header">
